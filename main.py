83843d51625bd0ea5fc4a93a53a00ed37b8e31b2
AlexaPan <xvspan@gmail.com>
2025-04-01 16:56:09 +0300

    add blog
    while not ended

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-# Задание 1
+# Задание 2
 #
-# Создайте новое приложение Flask, которое будет отображать
-# текущие дату и время на главной странице.
+# Создайте новое приложение Flask, создайте структуру проекта
+# с папками static и templates, в папке templates должны
+# быть 3 html файла: index, blog, contacts (главная страница,
+# страница блога и контакты). Заполните их информацией и выведите
+# силами flask сервера, используя функцию render_template()
+#
+# Обязательно на всех страницах сделайте меню, которое будет
+# работать именно при запуске проекта через flask
 
 from flask import Flask, render_template
-from datetime import datetime
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def show_datetime():
-    # Получаем текущее время и форматируем его
-    now = datetime.now()
+def app_cards():
+    return render_template("index.html")
 
-    return render_template("index1.html", current_time=now)
+@app.route('/blog/')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact/')
+def contacts():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
